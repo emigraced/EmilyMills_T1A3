27dbf87a63926fe6fc152adb3d037164dfd57fcb
emigraced <helloemilymills@gmail.com>
2020-12-13 17:17:54 +1000

    added ability to select from sims previously created (for 'continue playing' option)

--- src/index.rb
+++ src/index.rb
@@ -12,8 +12,6 @@ def ascii_title
   end
 end
 
-
-
 #saving created sims to the database file
 def save_created_sim(name, gender, life_stage, trait)
     sim_id = {:id => {:name => name, :gender => gender, :life_stage => life_stage, :trait => trait}}
@@ -21,13 +19,24 @@ def save_created_sim(name, gender, life_stage, trait)
     puts "Hooray, you've successfully created #{name}!"
 end
 
-#arrays for the menu method
+#selecting from saved sims
+def read_sim_library
+log = File.open("../data/database.yml")
+saved_sims_options = []
+YAML::load_stream( log ) {|doc| 
+saved_sims_options << "#{doc[:id][:name]}"
+}
+return saved_sims_options
+end
+
+#vars for the menu
 home_menu_options = ["Create a Sim!", "Continue playing", "Read the instructions", "Exit"]
 gender_options = ["female", "male"]
 life_stage_options = ["baby", "child", "adult", "elder"]
 trait_options = ["friendly", "mean"]
+sim_library = YAML.load(File.read("../data/database.yml"))
 
-#welcome menu
+#menu
 puts ascii_title
 puts "Welcome to The Sims: Command Line Edition!"
 sleep(0.5)
@@ -58,16 +67,18 @@ when home_menu_options[0]
     input_name = gets.strip
     save_created_sim(input_name, input_gender, input_life_stage, input_trait)
 when home_menu_options[1]
-    #if database == empty, puts "You haven't created any Sims yet. Please return to the main menu and make a different selection."
-    #else:
-    select_sim = prompt.select("Which Sim would you like to play?", YAML.load(File.read("../data/database.yml"))[:id][:name])
+    if sim_library.nil? #not working
+        puts "Oops! You haven't created any Sims yet. Please make a different selection."
+    else
+    select_sim = prompt.select("Which Sim would you like to play?", read_sim_library)
+    end
 when home_menu_options[2]
     #puts rules
 end
 end
 
-#need classes! sims, interactions, households, summary
-#when this game is ready to be shared, you need to replace your database.yml file with a template (because you don't want other people to have your database!). Maybe a git-ignore on the yml file?
-#add option for erasing saved file and starting again? Maybe a second array that's applied instead if the database.yml file is empty?
 
+
+#when this game is ready to be shared, you need to replace your database.yml file with a template (because you don't want other people to have your database!). Maybe a git-ignore on the yml file?
+#add option for erasing saved file and starting again? Maybe this could be a command line argument that wipes the yaml file
 
