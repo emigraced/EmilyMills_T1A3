4d71741bb2a2a725418661dab24d3ba6d21a3d7c
emigraced <helloemilymills@gmail.com>
2020-12-17 23:31:59 +1000

    sim gameplay validation success. sim name validation and deleting sims functions in the works. put methods into a separate ruby doc. implemented tty-color.

--- src/index.rb
+++ src/index.rb
@@ -1,15 +1,17 @@
 #required
 require "yaml"
 require "tty-prompt"
+require "tty-color"
 require "pastel"
 require "tty-spinner"
+require "./methods.rb"
 
 #tty inits
-prompt = TTY::Prompt.new(symbols: {marker: "♦"})
+prompt = TTY::Prompt.new(symbols: {marker: "♦"}, active_color: :cyan)
 pastel = Pastel.new
 
-#variables for the menu
-home_menu_options = ["Create a Sim!", "Choose a Sim to play", "Read the instructions", "Exit"]
+#arrays for the menu options
+home_menu_options = ["Create a Sim!", "Choose a Sim to play", "Delete Sims", "Read the instructions", "Exit"]
 gender_options = ["female", "male"]
 life_stage_options = ["baby", "child", "adult", "elder"]
 trait_options = ["friendly", "mean"]
@@ -17,54 +19,8 @@ interaction_options = ["Become friends", "Become enemies"]
 $outcome_options = ["Success!", "Uh oh..."]
 
 #probabilities
-friendly_probability = [[0, 0, 0, 0, 0], [0, 0, 0, 1, 1]]  #friendly sim choosing to become friends will be 100% successful, friendly sim trying to become enemies will be 60% successful
-mean_probability = [[0, 0, 0, 1, 1], [0, 0, 0, 0, 0]]  #mean sim choosing to become friends will be 60% successful, mean sim trying to become enemies will be 100% successful
-
-#ascii "The Sims" title
-def ascii_title
-  File.readlines("../docs/ascii_title.txt") do |line|
-    puts line
-  end
-end
-
-#saving created sims to the database file
-def save_created_sim(name, gender, life_stage, trait)
-    sim_id = {:id => {:name => name, :gender => gender, :life_stage => life_stage, :trait => trait}}
-    File.open("../data/database.yml", "a+") { |file| file.write(sim_id.to_yaml) }
-    puts "Hooray, you've successfully created #{name}!"
-end
-
-#selecting from saved sims
-def read_sim_library
-log = File.read("../data/database.yml")
-saved_sims_options = []
-YAML::load_stream(log) do |doc| 
-saved_sims_options << "#{doc[:id][:name]}"
-end
-return saved_sims_options
-end
-
-#finding the sim's trait for probability calculations
-def find_trait(sim)
-#open yaml file, look for the ID that matches the sim's name (which has been passed in), and look for the trait associated with that ID
-    log = File.read("../data/database.yml")
-    YAML::load_stream(log) do |doc| 
-            if sim == doc[:id][:name]
-            $selected_sim_trait = doc[:id][:trait]
-            else
-                next 
-            end
-        return $selected_sim_trait
-    end
-end
-
-#randomise interaction response
-def probability_generator(array)
-    rand_num = rand(5)
-    rand_index_generation = array[rand_num]
-    outcome = $outcome_options[rand_index_generation]
-    return outcome
-end
+friendly_probability = [[0, 0, 0, 0, 0], [0, 0, 0, 1, 1]] #friendly sim choosing to become friends will be 100% successful, friendly sim trying to become enemies will be 60% successful
+mean_probability = [[0, 0, 0, 1, 1], [0, 0, 0, 0, 0]] #mean sim choosing to become friends will be 60% successful, mean sim trying to become enemies will be 100% successful
 
 #menu
 puts ascii_title
@@ -74,7 +30,7 @@ user_selection = 0
 until user_selection == home_menu_options[-1]
 user_selection = prompt.select("What would you like to do?", home_menu_options)
 case user_selection
-when home_menu_options[0]
+when home_menu_options[0] #create a sim
     sleep(0.5)
     input_gender = prompt.select("Choose your Sim's gender:", gender_options)
     if input_gender == "female"
@@ -84,7 +40,6 @@ when home_menu_options[0]
         sleep(0.5)
         input_life_stage = prompt.select("What's his life stage?", life_stage_options)
     end
-    sleep(0.5)
     if input_gender == "female"
         sleep(0.5)
         input_trait = prompt.select("What kind of Sim will she be?", trait_options)
@@ -95,13 +50,20 @@ when home_menu_options[0]
     sleep(0.5)
     puts "Finally, give your Sim a first name:"
     input_name = gets.strip.capitalize
-    #make a way to not double up on names
+    # name_check = does_name_exist(input_name)
+    # if name_check == false
     save_created_sim(input_name, input_gender, input_life_stage, input_trait)
+    # else
+    #     puts "You have already saved a Sim with that name! Please try again with a different name, or delete the original Sim."
+    # end
+    # #need to loop back and allow user to enter a different name
+    
     sleep(1)
-when home_menu_options[1]
+when home_menu_options[1] #choose a sim to play
     sim_library = YAML.load(File.read("../data/database.yml"))
-    if sim_library == false
-        puts pastel.bright_yellow("Oops! You haven't created any Sims yet. Please make a different selection.")
+    if sim_library == false || read_sim_library.size < 2
+        puts pastel.bright_yellow("Oops! You need to create at least two Sims first. Please make a different selection.")
+        next
     else
     selected_sim = prompt.select("Please select a Sim", read_sim_library)
     end
@@ -118,10 +80,13 @@ when home_menu_options[1]
     chosen_interaction = prompt.select("How would you like #{selected_sim} to interact with #{recipient_sim}?", interaction_options)
     find_trait(selected_sim) 
         if chosen_interaction == interaction_options[0] && $selected_sim_trait == "friendly" #friendly selects become friends
-            puts probability_generator(friendly_probability[0])
+            result = probability_generator(friendly_probability[0])
+            puts result
+            save_interactions(result, selected_sim, recipient_sim)
         elsif chosen_interaction == interaction_options[1] && $selected_sim_trait == "friendly" #friendly selects become enemies
             result = probability_generator(friendly_probability[1])
             puts result
+            save_interactions(result, selected_sim, recipient_sim)
             sleep(0.5)
                 if result == $outcome_options[1]
                 puts "Because #{selected_sim} is friendly, they didn't have what it takes to be mean this time. #{selected_sim} and #{recipient_sim} are now friends :)"
@@ -129,18 +94,36 @@ when home_menu_options[1]
         elsif chosen_interaction == interaction_options[0] && $selected_sim_trait == "mean" #mean selects become friends
             result = probability_generator(mean_probability[0])
             puts result
+            save_interactions(result, selected_sim, recipient_sim)
             sleep(0.5)
                 if result == $outcome_options[1]
                 puts "Because #{selected_sim} is mean, they failed to make friends this time! #{selected_sim} and #{recipient_sim} are now enemies :("
                 end
         elsif chosen_interaction == interaction_options[1] && $selected_sim_trait == "mean" #mean selects become enemies 
-            puts probability_generator(mean_probability[1])
+            result = probability_generator(mean_probability[1])
+            puts result
+            if result == $outcome_options[0]
+                save_interactions(enemies, selected_sim, recipient_sim)
+            elsif result == $outcome_options[1]
+                save_interactions(friends, selected_sim, recipient_sim)
+            else
+                puts "Error"
+            end
         else
             puts "Oh dear, we've run into a problem with the app. Please contact the creator."
         end
     sleep(1)
-when home_menu_options[2]
-    #puts rules
+when home_menu_options[2] #delete sims
+    sim_library_status = read_sim_library
+    if sim_library_status == []
+    puts pastel.bright_yellow("Create some Sims first, and then you can come back here if you'd like to delete them.")
+    else
+    sim_to_delete = prompt.select("Which Sim would you like to delete? (Be careful! This action cannot be undone.)", read_sim_library) 
+    #delete_sim(sim_to_delete)
+    end
+    sleep(1)
+when home_menu_options[3] #read the instructions
+    #code
     sleep(1)
 end
 end
